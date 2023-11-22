c87994d4611c9aa59c9824566f5d3c37479256d6
Karthik Karavatt <karthikkaravatt@gmail.com>
2023-11-22 15:25:00 +0800

    Added the dialogue for adding list groups

--- ToDoAppConsole/ToDoView/ApplicationUi.cs
+++ ToDoAppConsole/ToDoView/ApplicationUi.cs
@@ -1,28 +1,39 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Terminal.Gui;
 using ToDoApplicationConsole.ToDoModel;
 
 namespace ToDoApplicationConsole.ToDoView
 {
+    /// <summary>
+    /// The is the front end ui for the application
+    /// </summary>
     internal class ApplicationUi
     {
-        private ScrollView _mainWindow;
-        private readonly List<FrameView> _frames = new List<FrameView>();
-        private ToDoListCollections _listCollections;
+        private readonly View _mainWindow;
+        private readonly ScrollView _contentWindow;
+        private readonly ToDoListCollections _listCollections;
         public ApplicationUi(ToDoListCollections toDoListCollections)
         {
             _listCollections = toDoListCollections;
-            _mainWindow = InitializeMainWindow();
+            (_mainWindow, _contentWindow) = InitializeMainWindow();
             SetKeyBinds();
         }
 
-        public ScrollView GetUi()
+        public View GetUi()
         {
             return _mainWindow;
         }
 
-        private static ScrollView InitializeMainWindow()
+        private static (View mainWindow, ScrollView conetnWindow) InitializeMainWindow()
         {
+            View view = new View()
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+            };
             ScrollView scroll = new ScrollView()
             {
                 X = 0,
@@ -37,13 +48,14 @@ namespace ToDoApplicationConsole.ToDoView
                 scroll.ContentSize = new Size(Application.Top.Frame.Width + 500, Application.Top.Frame.Height);
             };
             scroll.ContentSize = new Size(Application.Top.Frame.Width + 500, Application.Top.Frame.Height);
-            return scroll;
+            view.Add(scroll);
+            return (view, scroll);
 
         }
 
         private void SetKeyBinds()
         {
-            _mainWindow.KeyPress += (e) =>
+            _contentWindow.KeyPress += (e) =>
             {
 
                 switch (e.KeyEvent.Key)
@@ -60,12 +72,89 @@ namespace ToDoApplicationConsole.ToDoView
             };
         }
 
+        private static Dialog SetupAddToDoGroupDialogue()
+        {
+            int width = (int)Math.Round(Application.Top.Frame.Width * 0.40);
+            int height = (int)Math.Round(Application.Top.Frame.Height * 0.40);
+            Button okButton = new Button(ApplicationUiConstants.OkButtonName);
+            Button cancelButton = new Button(ApplicationUiConstants.CancelButtonName);
+            Dialog addToDoGroupDialog = new Dialog(ApplicationUiConstants.EnterGroupLabel, width, height)
+            {
+                X = Pos.Center(),
+                Y = Pos.Center()
+            };
+            TextField textField = new TextField()
+            {
+                Y = Pos.Center(),
+                X = Pos.Center(),
+                Width = Dim.Percent(70),
+
+            };
+            addToDoGroupDialog.Add(textField);
+            addToDoGroupDialog.AddButton(okButton);
+            addToDoGroupDialog.AddButton(cancelButton);
+            Application.Resized += (Application.ResizedEventArgs args) =>
+            {
+                width = (int)Math.Round(Application.Top.Frame.Width * 0.40);
+                height = (int)Math.Round(Application.Top.Frame.Height * 0.40);
+                addToDoGroupDialog.Width = width;
+                addToDoGroupDialog.Height = height;
+            };
+            return addToDoGroupDialog;
+
+        }
         private void AddToDoGroup()
         {
-            Button okButton = new Button("Ok");
-            Button cancelButton = new Button("Cancel");
-            Dialog addToDoGroupDialog = new Dialog("Quit", okButton, cancelButton);
+            Dialog addToDoGroupDialog = SetupAddToDoGroupDialogue();
+            List<Button> buttons = new List<Button>();
+            List<TextField> textFields = new List<TextField>();
+
+            foreach (var subview in addToDoGroupDialog.Subviews)
+            {
+                if (subview is View container)
+                {
+                    buttons.AddRange(container.Subviews.OfType<Button>());
+                    textFields.AddRange(container.Subviews.OfType<TextField>());
+                }
+            }
+
+            // the buttons with their Text property set to "Ok" or "Cancel"
+            var okButton = buttons.Find(button => button.Text == ApplicationUiConstants.OkButtonName);
+            var cancelButton = buttons.Find(button => button.Text == ApplicationUiConstants.CancelButtonName);
+            var textField = textFields[0];
+            if (okButton == null || cancelButton == null || textField == null)
+            {
+                throw new InvalidOperationException(ApplicationUiConstants.DialogButtonNotFoundError);
+            }
+            okButton.Clicked += () =>
+            {
+
+                string? groupName = textField.Text.ToString();
+                if (groupName != null)
+                {
+                    (bool success, string message) result = _listCollections.AddListGroup(groupName);
+                    Console.WriteLine(result);
+                }
+                _contentWindow.Enabled = true;
+                _contentWindow.FocusFirst();
+                _mainWindow.Remove(addToDoGroupDialog);
+
+
+
+            };
+            cancelButton.Clicked += () =>
+            {
+                // re-enable window and focus it
+                _contentWindow.Enabled = true;
+                _contentWindow.FocusFirst();
+                _mainWindow.Remove(addToDoGroupDialog);
+
+            };
+            _contentWindow.Enabled = false;
+            _mainWindow.Add(addToDoGroupDialog);
+
         }
+
 
 
     }
--- ToDoAppConsole/ToDoView/ApplicationUiConstants.cs
+++ ToDoAppConsole/ToDoView/ApplicationUiConstants.cs
@@ -3,5 +3,10 @@
     internal static class ApplicationUiConstants
     {
         public const int ToDoListSize = 500;
+        public const String OkButtonName = "Ok";
+        public const String CancelButtonName = "Cancel";
+        public const String EnterGroupLabel = "Enter group name";
+
+        public const String DialogButtonNotFoundError = "Dialog button not found";
     }
 }
